e976a9991d99b12b5c54b17a9ee6b28a158f3c8a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to: Use ConfirmationBehavior
+
+
+This example demonstrates how to use the ConfirmationBehavior.<br />Review <a href="https://documentation.devexpress.com/#WPF/CustomDocument17372">this documentation topic</a> to learn more.
+
+<br/>
+
+
---
+++ VB/ViewModel/MainViewModel.vb
@@ -0,0 +1,27 @@
+﻿Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm.DataAnnotations
+Imports DevExpress.Mvvm.POCO
+
+Namespace Example.ViewModel
+    <POCOViewModel> _
+    Public Class MainViewModel
+        Private SavedText As String
+        Public Overridable Property Text() As String
+        Public ReadOnly Property IsSaved() As Boolean
+            Get
+                Return Text = SavedText
+            End Get
+        End Property
+        Public Sub Save()
+            SavedText = Text
+            Me.RaisePropertyChanged(Function(x) x.IsSaved)
+        End Sub
+        Public Sub Close()
+            SavedText = ""
+            Text = ""
+        End Sub
+        Protected Sub OnTextChanged()
+            Me.RaisePropertyChanged(Function(x) x.IsSaved)
+        End Sub
+    End Class
+End Namespace
