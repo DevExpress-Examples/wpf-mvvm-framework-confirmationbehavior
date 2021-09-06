939efe2ea911d66a2c8c60a2b004f121458b9d1e
DevExpressExampleBot <fakeEmail>
2021-09-06 20:53:12 +0300

    Source auto update [skip ci]

--- VB/ViewModel/MainViewModel.vb
+++ VB/ViewModel/MainViewModel.vb
@@ -3,25 +3,25 @@ Imports DevExpress.Mvvm.DataAnnotations
 Imports DevExpress.Mvvm.POCO
 
 Namespace Example.ViewModel
-    <POCOViewModel> _
-    Public Class MainViewModel
-        Private SavedText As String
-        Public Overridable Property Text() As String
-        Public ReadOnly Property IsSaved() As Boolean
-            Get
-                Return Text = SavedText
-            End Get
-        End Property
-        Public Sub Save()
-            SavedText = Text
-            Me.RaisePropertyChanged(Function(x) x.IsSaved)
-        End Sub
-        Public Sub Close()
-            SavedText = ""
-            Text = ""
-        End Sub
-        Protected Sub OnTextChanged()
-            Me.RaisePropertyChanged(Function(x) x.IsSaved)
-        End Sub
-    End Class
+	<POCOViewModel>
+	Public Class MainViewModel
+		Private SavedText As String
+		Public Overridable Property Text() As String
+		Public ReadOnly Property IsSaved() As Boolean
+			Get
+				Return Text = SavedText
+			End Get
+		End Property
+		Public Sub Save()
+			SavedText = Text
+			Me.RaisePropertyChanged(Function(x) x.IsSaved)
+		End Sub
+		Public Sub Close()
+			SavedText = ""
+			Text = ""
+		End Sub
+		Protected Sub OnTextChanged()
+			Me.RaisePropertyChanged(Function(x) x.IsSaved)
+		End Sub
+	End Class
 End Namespace
