6c90c939448e126534d450d3ff5eb3fe184612aa
DevExpressExampleBot <fakeEmail>
2026-02-03 19:22:08 +0400

    Source auto update [skip ci]

--- VB/ViewModel/MainViewModel.vb
+++ VB/ViewModel/MainViewModel.vb
@@ -1,37 +1,40 @@
-﻿Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm
 Imports DevExpress.Mvvm.DataAnnotations
 
 Namespace Example.ViewModel
-	Public Class MainViewModel
-		Inherits ViewModelBase
 
-		Private savedText As String
+    Public Class MainViewModel
+        Inherits ViewModelBase
 
-		Public Property Text() As String
-			Get
-				Return GetValue(Of String)()
-			End Get
-			Set(ByVal value As String)
-				SetValue(value)
-				RaisePropertyChanged(NameOf(IsSaved))
-			End Set
-		End Property
-		Public ReadOnly Property IsSaved() As Boolean
-			Get
-				Return Text = savedText AndAlso Not String.IsNullOrEmpty(Text)
-			End Get
-		End Property
+        Private savedText As String
 
-		<Command>
-		Public Sub Save()
-			savedText = Text
-			RaisePropertyChanged(NameOf(IsSaved))
-		End Sub
+        Public Property Text As String
+            Get
+                Return GetValue(Of String)()
+            End Get
 
-		<Command>
-		Public Sub Close()
-			savedText = String.Empty
-			Text = String.Empty
-		End Sub
-	End Class
+            Set(ByVal value As String)
+                SetValue(value)
+                RaisePropertyChanged(NameOf(MainViewModel.IsSaved))
+            End Set
+        End Property
+
+        Public ReadOnly Property IsSaved As Boolean
+            Get
+                Return Equals(Text, savedText) AndAlso Not String.IsNullOrEmpty(Text)
+            End Get
+        End Property
+
+        <Command>
+        Public Sub Save()
+            savedText = Text
+            RaisePropertyChanged(NameOf(MainViewModel.IsSaved))
+        End Sub
+
+        <Command>
+        Public Sub Close()
+            savedText = String.Empty
+            Text = String.Empty
+        End Sub
+    End Class
 End Namespace
